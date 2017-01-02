7ac86ccd1b67f0aae8a8ec12ad71f7acc3acb193
Grace Robbins <gcrobbins2@gmail.com>
2017-01-02 11:52:17 -0600

    created index and detail views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[A-Za-z0-9_]+)/$', views.DetailView.as_view(), name='detail')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Item
+from django.utils import timezone
 
-from django.http import HttpResponse
 
+#def index(request):
+ #   return HttpResponse("Hello, world. This is the posts page")
 
-def index(request):
-    return HttpResponse("Hello, world. This is the posts page")
+class IndexView(generic.ListView):
+	template_name = 'posts/index.html'
+	context_object_name = 'latest_post_list'
+	
+	def get_queryset(self):
+		return Item.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:10]
+
+class DetailView(generic.DetailView):
+	model = Item
+	template_name = 'posts/detail.html'
+	
+	def get_queryset(self):
+		return Item.objects.filter(pub_date__lte=timezone.now())
+
+#def details(request):
+#	return HttpResponse("this is the detail page for a post")#
